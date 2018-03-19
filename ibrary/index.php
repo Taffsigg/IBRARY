7e44d120ac2b2c8a7e56b27a1ec2170ef609e7c4
mohanteja1 <mohantejachitturi1@gmail.com>
2018-03-20 00:07:03 +0530

    Add files via upload

---
+++ ibrary/index.php
@@ -0,0 +1,310 @@
+
+
+
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>IBRARY &mdash; </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="E LEARNING" />
+	<meta name="keywords" content="LIBRARY,ECOURSES" />
+	<meta name="author" content="MOHANTEJA" />
+
+	
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	DEVELOPES
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2">
+					<div id="fh5co-logo"><a href="index.php">iBRARY</a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">
+					<ul>
+						<li class="active"><a href="index.php">Home</a></li>
+						
+						<li class="has-dropdown">
+							<a href="services.php">services</a>
+							<ul class="dropdown">
+								<li><a href="#">Get started</a></li>
+								<li><a href="#">Instructor guide</a></li>
+								<li><a href="#">support</a></li>
+								<li><a href="#">community</a></li>
+								<li><a href="contact.php">career</a></li>
+							</ul>
+						</li>
+						
+						
+						<li><a href="../onlinecourse/index.php">Login</a></li>
+						<li><a href="signlog/signup.php">Signup</a></li>
+						<li><a href="work.php">Courses</a></li>
+						<li><a href="contact.php">Contact</a></li>
+						<li><a href="about.php">About</a></li>
+
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/glow.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>We are Ibrary</h1>
+							
+							<p><a href="work.php" class="btn btn-default">View Our Work</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	
+	
+	
+
+	<div id="fh5co-testimonial">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Testimonial</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 animate-box">
+					<div class="testimony">
+						<blockquote>
+							&ldquo;ibrary is very user friendly and usefull tool for learning &rdquo;
+						</blockquote>
+						<p class="author"><cite>John Doe</cite></p>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="testimony">
+						<blockquote>
+							&ldquo;best elearning platform available&rdquo;
+						</blockquote>
+						<p class="author"><cite>Bill gates</cite></p>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="testimony">
+						<blockquote>
+							&ldquo;advanced technology available&rdquo;
+						</blockquote>
+						<p class="author"><cite>Jane Doe</cite></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Newsletter</h2>
+					<p>for new offers and more courses please subscribe to our website</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<form class="form-inline"  method='post' >
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="text" name='email' class="form-control" id="email" placeholder="Email">
+                                 <br><br>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<button type="submit" name='submit' class="btn btn-default btn-block">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h3>The Company</h3>
+					<p>we’re improving lives through learning.
+Join millions of students and instructors in the world’s largest online learning marketplace</p>
+					<p><a href="about.php">Learn More</a></p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="about.php">About</a></li>
+						<li><a href="contact.php">Contact</a></li>
+						<li><a href="#">Terms</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Privacy</a></li>
+						<li><a href="index.php">Testimonials</a></li>
+						<li><a href="#">Handbook</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="about.php">Find Designers</a></li>
+						<li><a href="contact.php">Find Developers</a></li>
+						<li><a href="contact.php">Teams</a></li>
+						<li><a href="../onlinecourse/admin/index.php">admin login</a></li>
+
+					</ul>
+				</div>
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2018 IBRARY. All Rights Reserved.</small> 
+						<small class="block">Designed by BATCH16<a href="https://www.anits.edu.in/" target="_blank"> ANITS.COM</a> <a href="https://www.anits.edu.in/" target="_blank"></a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
+
+
+
+
+<?php
+
+include 'dbinclude/dbconnect.php';
+ 
+$conn = OpenCon();
+ 
+echo "Connected Successfully";
+ 
+ 
+if(isset($_POST['submit'])) {
+
+if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+      echo 'invalid';
+    }
+    else
+    {
+    $sql="INSERT INTO  `emailssubcription` VALUES('$email')";
+   // echo '\nemail subscription Successfully';
+    if ($conn->query($sql) === TRUE) {
+  //  echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    }
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+CloseCon($conn);
+ 
+?>
---
+++ ibrary/services.php
@@ -0,0 +1,322 @@
+<!DOCTYPE HTML>
+<html>
+	<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>services &mdash; </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="E LEARNING" />
+	<meta name="keywords" content="LIBRARY,ECOURSES" />
+	<meta name="author" content="MOHANTEJA" />
+
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2">
+					<div id="fh5co-logo"><a href="index.php">iBRARY</a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">
+					<ul>
+						<li class="active"><a href="index.php">Home</a></li>
+						
+						<li class="has-dropdown">
+							<a href="services.php">services</a>
+							<ul class="dropdown">
+								<li><a href="#">Get started</a></li>
+								<li><a href="#">Instructor guide</a></li>
+								<li><a href="#">support</a></li>
+								<li><a href="#">community</a></li>
+								<li><a href="contact.php">career</a></li>
+							</ul>
+						</li>
+						
+						
+						<li><a href="../onlinecourse/index.php">Login</a></li>
+						<li><a href="signlog/signup.php">Signup</a></li>
+						<li><a href="work.php">Courses</a></li>
+						<li><a href="contact.php">Contact</a></li>
+						<li><a href="about.php">About</a></li>
+
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(images/img_bg_2.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Services</h1>
+							<h2></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<div id="fh5co-services-section">
+		<div class="container">
+			<div class="col-md-4 col-sm-4">
+				<div class="feature-center">
+					<span class="icon">
+						<i class="icon-eye"></i>
+					</span>
+					<h3>Getting started</h3>
+					<p><br/><br/>
+
+					go through our online tutorial how to get started with ibrary courses,community etc...<br/><br/></p>
+					<p><a href="#" class="btn btn-primary">Learn More</a></p>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="feature-center">
+					<span class="icon">
+						<i class="icon-command"></i>
+					</span>
+					<h3>instructor guide</h3>
+					<p><br/><br/>
+
+						If you are an instructor please go through this tutorial and guide lines<br/><br/></p></p>
+					<p><a href="#" class="btn btn-primary">Learn More</a></p>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="feature-center">
+					<span class="icon">
+						<i class="icon-power"></i>
+					</span>
+					<h3>community</h3>
+					<p>We are a meritocracy, not a democracy. That means we try to enable the people best equipped to lead, then we let them lead with confidence.Join our online community and help yourselves. </p>
+					<p><a href="#" class="btn btn-primary">Learn More</a></p>
+				</div>
+			</div>
+
+			<div class="col-md-4 col-sm-4">
+				<div class="feature-center">
+					<span class="icon">
+						<i class="icon-brush"></i>
+					</span>
+					<h3>career</h3>
+					<p>change the future of learning .Come join us</p>
+					<p><a href="#" class="btn btn-primary">Learn More</a></p>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="feature-center">
+					<span class="icon">
+						<i class="icon-video2"></i>
+					</span>
+					<h3>Video Editing</h3>
+					<p>view our video editing tool</p>
+					<p><a href="#" class="btn btn-primary">Learn More</a></p>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="feature-center">
+					<span class="icon">
+						<i class="icon-help2"></i>
+					</span>
+					<h3>24/7 Help &amp; Support</h3>
+					<p>How may we help you?</p>
+					<p><a href="#" class="btn btn-primary">Learn More</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Newsletter</h2>
+					<p>for new offers and more courses please subscribe to our website</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<form class="form-inline"  method='post' >
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="text" name='email' class="form-control" id="email" placeholder="Email">
+                                 <br><br>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<button type="submit" name='submit' class="btn btn-default btn-block">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h3>The Company</h3>
+					<p>we’re improving lives through learning.
+Join millions of students and instructors in the world’s largest online learning marketplace</p>
+					<p><a href="about.php">Learn More</a></p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="about.php">About</a></li>
+						<li><a href="contact.php">Contact</a></li>
+						<li><a href="#">Terms</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Privacy</a></li>
+						<li><a href="index.php">Testimonials</a></li>
+						<li><a href="#">Handbook</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="about.php">Find Designers</a></li>
+						<li><a href="contact.php">Find Developers</a></li>
+						<li><a href="contact.php">Teams</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2018 IBRARY. All Rights Reserved.</small> 
+						<small class="block">Designed by BATCH16<a href="https://www.anits.edu.in/" target="_blank"> ANITS.COM</a> <a href="https://www.anits.edu.in/" target="_blank"></a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
+<?php
+
+include 'dbinclude/dbconnect.php';
+ 
+$conn = OpenCon();
+ 
+//echo "Connected Successfully";
+ 
+ 
+if(isset($_POST['submit'])) {
+
+if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+      echo 'invalid';
+    }
+    else
+    {
+    $sql="INSERT INTO  `emailssubcription` VALUES('$email')";
+   // echo '\nemail subscription Successfully';
+    if ($conn->query($sql) === TRUE) {
+   // echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    }
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+CloseCon($conn);
+ 
+?>
