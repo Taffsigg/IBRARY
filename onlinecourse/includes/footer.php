eea210865614fc3261784e27688ea547ab589f2d
mohanteja1 <mohantejachitturi1@gmail.com>
2018-03-20 00:00:58 +0530

    Add files via upload

---
+++ onlinecourse/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2018 IBRARY. All Rights Reserved. <a href="https://www.anits.edu.in/" target="_blank">ANITS.COM</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
